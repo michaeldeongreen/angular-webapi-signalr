3abb74de74a76758dff918566ff30119f8d62eff
michaeldeongreen <michaeldeongreen@gmail.com>
2020-06-05 12:06:07 -0500

    First Commit

---
+++ Backend/SignalR/Startup.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SignalR.Hubs;
+
+namespace SignalR
+{
+    public class Startup
+    {
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHostedService<DashboardHostedService>();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddCors(c => {
+                c.AddPolicy(name: MyAllowSpecificOrigins,
+                                builder =>
+                                {
+                                    builder.WithOrigins("https://as-mgreen-signalr-frontend-dev.azurewebsites.net")
+                                    .AllowCredentials()
+                                    .AllowAnyHeader();
+                                });
+            });
+            services.AddSignalR();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors(MyAllowSpecificOrigins);
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints => {
+                endpoints.MapHub<NotificationHub>("/notificationHub");
+            });
+
+        }
+    }
+}
---
+++ Frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  hubUrl: 'https://as-mgreen-signalr-backend-dev.azurewebsites.net/notificationHub'
+};
